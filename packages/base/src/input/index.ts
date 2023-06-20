de99a1750b9f5b19e0c595af68bcb6cb9af81990
利浩 <chenlihao@shein.com>
2023-06-20 16:28:22 +0800

    feat: 把 InputGroup 迁移到 base 了

--- packages/base/src/input/index.ts
+++ packages/base/src/input/index.ts
@@ -6,3 +6,6 @@ export type { InputPasswordProps } from './input-password.type';
 
 export { default as InputNumber } from './input-number';
 export type { InputNumberProps } from './input-number.type';
+
+export { default as InputGroup } from './input-group';
+export type { InputGroupProps } from './input-group.type';
--- packages/shineout/src/input/group.tsx
+++ packages/shineout/src/input/group.tsx
@@ -1,71 +1,9 @@
-// @flow
 import * as React from 'react';
-import { ReactNode, Children, cloneElement, useRef } from 'react';
+import { InputGroup } from '@sheinx/base';
 import { useInputStyle } from '@sheinx/shineout-style';
-import classNames from 'classnames';
+import { GroupProps } from './group.type';
 
-type Props = {
-  children?: ReactNode;
-  className?: string;
-  style?: React.CSSProperties;
-  size?: 'small' | 'large' | 'default';
-  disabled?: boolean;
-};
-export default (props: Props) => {
+export default (props: GroupProps) => {
   const jssStyle = useInputStyle();
-  const [focus, setFocus] = React.useState(false);
-  const ref = useRef({
-    eventMap: new WeakMap(),
-    propsMap: new WeakMap(),
-  });
-  const getProps = (child: React.ReactElement) => {
-    if (
-      !['ShineoutInput', 'ShineoutInputNumber', 'ShineoutInputPassword'].includes(
-        (child?.type as any)?.displayName,
-      )
-    )
-      return {};
-    ref.current.propsMap.set(child, {
-      onFocus: child.props.onFocus,
-      onBlur: child.props.onBlur,
-    });
-    if (!ref.current.eventMap.get(child)) {
-      ref.current.eventMap.set(child, {
-        onFocus: (...args: any) => {
-          setFocus(true);
-          ref.current.propsMap.get(child)?.onFocus?.(args);
-        },
-        onBlur: (...args: any) => {
-          setFocus(false);
-          ref.current.propsMap.get(child)?.onBlur?.(args);
-        },
-        inGroup: true,
-      });
-    }
-    return ref.current.eventMap.get(child) || {};
-  };
-
-  const { children, className, style, size, disabled } = props;
-  const rootClass = classNames({
-    [jssStyle.group]: true,
-    [jssStyle.groupSmall]: size === 'small',
-    [jssStyle.groupLarge]: size === 'large',
-    [jssStyle.groupFocus]: size === 'large',
-    [jssStyle.groupDisabled]: !!disabled,
-    [className!]: !!props.className,
-    [jssStyle.groupFocus]: focus,
-  });
-  return (
-    <div className={rootClass} style={style}>
-      {Children.toArray(children).map((child, i) => {
-        if (typeof child === 'string') {
-          return <span key={i}>{child}</span>;
-        }
-        if (React.isValidElement(child)) {
-          return cloneElement(child, { ...getProps(child), disabled });
-        }
-        return <span key={i}>{child}</span>;
-      })}
-    </div>
-  );
+  return <InputGroup {...props} jssStyle={jssStyle} />;
 };
