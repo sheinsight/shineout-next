3b6af1306f7d5a67464f1d13893c5f713c401309
利浩 <chenlihao@shein.com>
2023-06-26 16:09:44 +0800

    feat: 完成 RadioGroup 接入 useFieldCommon

--- packages/shineout/src/radio/group.tsx
+++ packages/shineout/src/radio/group.tsx
@@ -1,10 +1,18 @@
-import { RadioGroupProps } from './group.type';
+import { BaseRadioGroupProps, RadioGroupProps } from './group.type';
 import { RadioGroup as UnStyledRadioGroup } from '@sheinx/base';
 import { useRadioStyle } from '@sheinx/shineout-style';
+import useFieldCommon from '../hooks/use-field-common';
 
-const BaseRadioGroup = <DataItem, Value>(props: RadioGroupProps<DataItem, Value>) => {
+const BaseRadioGroup = <DataItem, Value>(props: BaseRadioGroupProps<DataItem, Value>) => {
   const jssStyle = useRadioStyle();
   return <UnStyledRadioGroup {...props} jssStyle={jssStyle} />;
 };
 
-export default BaseRadioGroup;
+const RadioGroupWithField = <DataItem, Value>(props: RadioGroupProps<DataItem, Value>) => {
+  return useFieldCommon<
+    BaseRadioGroupProps<DataItem, Value>,
+    BaseRadioGroupProps<DataItem, Value>['value']
+  >(props, BaseRadioGroup);
+};
+
+export default RadioGroupWithField;
--- packages/shineout/src/radio/group.type.ts
+++ packages/shineout/src/radio/group.type.ts
@@ -1,5 +1,11 @@
 import { RadioGroupProps as UnStyledRadioGroupProps } from '@sheinx/base';
-export type RadioGroupProps<DataItem, Value> = Omit<
+import { GetWithFieldProps } from '../hooks/use-field-common';
+export type BaseRadioGroupProps<DataItem, Value> = Omit<
   UnStyledRadioGroupProps<DataItem, Value>,
   'jssStyle'
+>;
+
+export type RadioGroupProps<DataItem, Value> = GetWithFieldProps<
+  BaseRadioGroupProps<DataItem, Value>,
+  BaseRadioGroupProps<DataItem, Value>['value']
 >;
