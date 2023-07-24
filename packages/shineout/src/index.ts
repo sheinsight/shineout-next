811c39b55269cb6be947134fe9fa305a379c36a5
利浩 <chenlihao@shein.com>
2023-07-24 13:39:23 +0800

    feat: 增加主题

--- packages/shineout/src/index.ts
+++ packages/shineout/src/index.ts
@@ -2,7 +2,7 @@ export { default as Input } from './input';
 export { default as Textarea } from './textarea';
 export { default as Form } from './form';
 export { default as Radio } from './radio';
-export { default as Checkbox } from './Checkbox';
+export { default as Checkbox } from './checkbox';
 export { default as Rule } from './rule';
 export { default as Dropdown } from './dropdown';
 export { default as Button } from './button';
