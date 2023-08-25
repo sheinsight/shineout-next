455f43a81aaed53cb5e271eca7ea1bab82b7e4d5
zyfwudi <leonardzhao@shein.com>
2023-08-25 16:29:03 +0800

    feat: temp

--- packages/shineout/src/dropdown/__test__/dropdown.spec.tsx
+++ packages/shineout/src/dropdown/__test__/dropdown.spec.tsx
@@ -2,8 +2,19 @@ import { render, cleanup, screen } from '@testing-library/react';
 import Dropdown from '..';
 import { TYPE } from 'shineout';
 import mountTest from '../../tests/mountTest';
-import { baseTest, snapshotTest } from '../../tests/utils';
+import { baseTest, snapshotTest, displayTest } from '../../tests/utils';
 import DropdownBase from '../__example__/001-base';
+import DropdownHover from '../__example__/002-hover';
+import DropdownPosition from '../__example__/003-position';
+import DropdownButton from '../__example__/004-button';
+import DropdownChildren from '../__example__/005-children';
+import DropdownGroup from '../__example__/006-group';
+import DropdownDivider from '../__example__/007-divider';
+import DropdownColumns from '../__example__/008-columns';
+import DropdownIcon from '../__example__/009-icon';
+import DropdownSize from '../__example__/010-size';
+import DropdownSplit from '../__example__/011-split';
+import DropdownAbsolute from '../__example__/012-absolute';
 
 const SO_PREFIX = 'dropdown';
 const dropdownClassName = `.${SO_PREFIX}-wrapper-0-2-1`;
@@ -36,10 +47,9 @@ const data: DropdownItem[] = [
 ];
 
 afterEach(cleanup);
-mountTest(Dropdown);
+mountTest(<Dropdown data={data} />);
 describe('Dropdown[Base]', () => {
-  //TODO: 没有displayname
-  // displayTest(Dropdown, '')
+  displayTest(Dropdown, 'ShineoutDropdown');
   baseTest(
     <Dropdown
       placeholder='Dropdown'
@@ -50,6 +60,17 @@ describe('Dropdown[Base]', () => {
     dropdownClassName,
   );
   snapshotTest(<DropdownBase />);
+  snapshotTest(<DropdownHover />, 'about hover');
+  snapshotTest(<DropdownPosition />, 'about position');
+  snapshotTest(<DropdownButton />, 'about button');
+  snapshotTest(<DropdownChildren />, 'about children');
+  snapshotTest(<DropdownGroup />, 'about group');
+  snapshotTest(<DropdownDivider />, 'about divider');
+  snapshotTest(<DropdownColumns />, 'about columns');
+  snapshotTest(<DropdownIcon />, 'about icon');
+  snapshotTest(<DropdownSize />, 'about size');
+  snapshotTest(<DropdownSplit />, 'about split');
+  snapshotTest(<DropdownAbsolute />, 'about absolute');
   test('should render', () => {
     const clickFn = jest.fn();
     render(<Dropdown placeholder='Dropdown' data={data} onClick={clickFn} />);
