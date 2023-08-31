eed7e6333b218b9dffe3dbd6f79cc443aba3881e
zyfwudi <leonardzhao@shein.com>
2023-08-31 17:06:55 +0800

    feat: temp

---
+++ packages/shineout/src/tabs/__test__/tabs.spec.tsx
@@ -0,0 +1,14 @@
+// import { cleanup } from '@testing-library/react'
+// import Tabs from '..'
+// import mountTest from '../../tests/mountTest'
+// import { render } from '@sheinx/hooks/dist/cjs/utils'
+
+// const SO_PREFIX = 'tabs'
+
+// afterEach(cleanup)
+// describe('Tabs[Base]', () => {
+//   mountTest(Tabs)
+//   test('should render', () => {
+//     render(<Tabs />)
+//   })
+// })
