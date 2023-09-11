e02f03e9a495e624d82adc41710d74fd47750989
颜宇浩 <yanyuhao@shein.com>
2023-09-11 09:59:51 +0800

    feat: 修改 image 验收问题

--- packages/shineout/src/image/__example__/s-005-fit.tsx
+++ packages/shineout/src/image/__example__/s-005-fit.tsx
@@ -11,6 +11,8 @@ import React from 'react';
 import { Image } from 'shineout';
 
 export default () => {
+  const fit = ['fill', 'center', 'fit', 'stretch'];
+
   return (
     <div
       style={{
@@ -18,30 +20,21 @@ export default () => {
         display: 'flex',
       }}
     >
-      <Image
-        fit='fill'
-        width={128}
-        height={128}
-        src='https://raw.githubusercontent.com/sheinsight/shineout-static/main/shineout-next/images/image/s-01.png'
-      ></Image>
-      <Image
-        fit='center'
-        width={128}
-        height={128}
-        src='https://raw.githubusercontent.com/sheinsight/shineout-static/main/shineout-next/images/image/s-01.png'
-      ></Image>
-      <Image
-        fit='fit'
-        width={128}
-        height={128}
-        src='https://raw.githubusercontent.com/sheinsight/shineout-static/main/shineout-next/images/image/s-01.png'
-      ></Image>
-      <Image
-        fit='stretch'
-        width={128}
-        height={128}
-        src='https://raw.githubusercontent.com/sheinsight/shineout-static/main/shineout-next/images/image/s-01.png'
-      ></Image>
+      {fit.map((item, index) => {
+        return (
+          <div key={index} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Image
+              fit={item as any}
+              width={128}
+              height={128}
+              src='https://raw.githubusercontent.com/sheinsight/shineout-static/main/shineout-next/images/image/s-01.png'
+            ></Image>
+            <div style={{ textAlign: 'center', color: '#141737', fontSize: 12, marginTop: 5 }}>
+              {item}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
