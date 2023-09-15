d615401125434a829852e2a475fef52615a77e31
zyfwudi <leonardzhao@shein.com>
2023-09-15 16:01:47 +0800

    feat: begin cypress test about add cypress.config.ts and more

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:4000',
+    // specPattern: `cypress/e2e/${nycConfig.target}/*.cy.{js, jsx, ts, tsx}`,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+  },
+});
--- scripts/merge-coverage.js
+++ scripts/merge-coverage.js
@@ -2,8 +2,25 @@
  * This script can merge the coverage reports from Cypress and Jest by testing-library into a single one
  */
 
-// const { execSync } = require('child_process')
-// const fs = require('fs-extra')
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
 
-// const REPORTS_FOLDER = 'reports'
-// const FINAL_OUTPUT_FOLDER = 'coverage'
+const REPORTS_FOLDER = 'reports';
+const FINAL_OUTPUT_FOLDER = 'coverage';
+
+fs.emptyDirSync(REPORTS_FOLDER);
+fs.copyFileSync('cypress-coverage/coverage-final.json', `${REPORTS_FOLDER}/from-cypress.json`);
+fs.copyFileSync('coverage/coverage-final.json', `${REPORTS_FOLDER}/from-jest.json`);
+
+fs.emptyDirSync('.nyc_output');
+fs.emptyDirSync(FINAL_OUTPUT_FOLDER);
+
+execSync(`nyc merge ${REPORTS_FOLDER} && mv coverage.json .nyc_output/out.json`, {
+  stdio: 'inherit',
+});
+execSync(
+  `nyc report --reporter lcov --report-dir ${FINAL_OUTPUT_FOLDER} --temp-dir ./.nyc_output`,
+  {
+    stdio: 'inherit',
+  },
+);
