8ac6df6c5077672356acbc842a641a014e6bc8fa
zyfwudi <leonardzhao@shein.com>
2023-09-15 14:04:31 +0800

    feat: temp

---
+++ scripts/merge-coverage.js
@@ -0,0 +1,9 @@
+/**
+ * This script can merge the coverage reports from Cypress and Jest by testing-library into a single one
+ */
+
+// const { execSync } = require('child_process')
+// const fs = require('fs-extra')
+
+// const REPORTS_FOLDER = 'reports'
+// const FINAL_OUTPUT_FOLDER = 'coverage'
