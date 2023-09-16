91e15fc677e5447b758b7bcb5e80d5f9a9a88a9b
zyfwudi <leonardzhao@shein.com>
2023-09-16 10:08:24 +0800

    feat: temp

---
+++ packages/shineout/src/tooltip/__test__/tooltip.spec.tsx
@@ -0,0 +1,10 @@
+// import { render, cleanup } from '@testing-library/react'
+// import '@testing-library/jest-dom'
+// import Tooltip from '..'
+// import mountTest from '../../tests/mountTest'
+
+// afterEach(cleanup)
+// // mountTest(<Tooltip />)
+// describe('Tooltip[Base]', () => {
+//   render(<Tooltip />)
+// })
