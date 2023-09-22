38da71e577ceab8c0b392e8fb8ec97feadfd04be
zyfwudi <leonardzhao@shein.com>
2023-09-22 16:37:23 +0800

    fix: revise unStyledDescriptions name

--- packages/shineout/src/descriptions/descriptions.tsx
+++ packages/shineout/src/descriptions/descriptions.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { Descriptions as UnStyledCheckbox } from '@sheinx/base';
+import { Descriptions as UnStyledDescriptions } from '@sheinx/base';
 import { useDescriptionsStyle } from '@sheinx/shineout-style';
 import { DescriptionsProps } from './descriptions.type';
 
@@ -7,7 +7,7 @@ const Descriptions = (props: DescriptionsProps) => {
   const descriptionsStyle = useDescriptionsStyle();
   const jssStyle = useMemo(() => ({ descriptions: descriptionsStyle }), [descriptionsStyle]);
 
-  return <UnStyledCheckbox jssStyle={jssStyle} {...props} />;
+  return <UnStyledDescriptions jssStyle={jssStyle} {...props} />;
 };
 
 export default Descriptions;
