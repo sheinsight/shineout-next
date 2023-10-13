efb08490d834dcb5e7e5371e953d6e43503e2386
zyfwudi <leonardzhao@shein.com>
2023-10-13 16:50:19 +0800

    feat: temp

--- packages/shineout/src/message/__test__/message.spec.tsx
+++ packages/shineout/src/message/__test__/message.spec.tsx
@@ -1,4 +1,4 @@
-import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
+import { render, cleanup, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Message from '..';
 import { Button } from 'shineout';
@@ -9,6 +9,7 @@ import {
   delay,
   textContentTest,
 } from '../../tests/utils';
+// import MessageBase from '../__example__/01-base'
 
 const SO_PREFIX = 'message';
 const originClasses = ['wrapper', 'item', 'message'];
@@ -24,14 +25,21 @@ const originAlertItemClasses = ['alert', 'info', 'success', 'warning', 'danger',
 const {
   content: contentClassName,
   alert: alertClassName,
-  success: successClassName,
   close: closeClassName,
+  ...rest
 } = createClassName(SO_PREFIX_Alert, originAlertClasses, originAlertItemClasses);
+
+const typeClassNameMap: { [key: string]: string } = {
+  info: 'info',
+  success: 'success',
+  warn: 'warning',
+  error: 'danger',
+};
 
 const MessageTest = () => (
   <Button
     onClick={() => {
-      Message.success('Test');
+      Message.show('Test');
     }}
   >
     Test
@@ -57,13 +65,14 @@ describe('Message[Base]', () => {
     fireEvent.click(container.querySelector('button')!);
     await waitFor(async () => {
       await delay(200);
+      screen.debug();
       const messageWrapper = document.querySelector(wrapper)!;
       attributesTest(messageWrapper, 'data-soui-position', 'top');
       const messageItem = messageWrapper.querySelector(item)!;
       classContentTest(messageItem, itemShow);
       const messageMain = messageItem.querySelector(message)!;
       classContentTest(messageMain, alertClassName);
-      classContentTest(messageMain, successClassName);
+      classContentTest(messageMain, rest.warning);
       textContentTest(messageMain.querySelector(contentClassName)!, 'Test');
       const messageClose = messageMain.querySelector(closeClassName)!;
       fireEvent.click(messageClose);
@@ -84,5 +93,159 @@ describe('Message[Base]', () => {
       },
       { timeout: 10000 },
     );
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelectorAll(item).length).toBe(1);
+    });
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelectorAll(item).length).toBe(2);
+      expect(document.querySelector(wrapper)).toMatchSnapshot();
+    });
+    await waitFor(
+      async () => {
+        await delay(3200);
+        expect(document.querySelectorAll(item).length).toBe(0);
+      },
+      { timeout: 10000 },
+    );
+  });
+  test.each(['info', 'success', 'warn', 'error'])(
+    'should render when set method is %s',
+    async (type) => {
+      const { container } = render(
+        <Button
+          onClick={() => {
+            // @ts-ignore
+            Message[type](`${type}`, 0);
+          }}
+        >
+          Test
+        </Button>,
+      );
+      fireEvent.click(container.querySelector('button')!);
+      await waitFor(async () => {
+        await delay(200);
+        classContentTest(document.querySelector(message)!, rest[typeClassNameMap[type]]);
+        fireEvent.click(document.querySelector(closeClassName)!);
+      });
+      await waitFor(async () => {
+        await delay(200);
+        expect(document.querySelector(wrapper)).not.toBeInTheDocument();
+      });
+    },
+  );
+  test('should render when set method is close', async () => {
+    Message.success('success', 0);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelectorAll(item).length).toBe(1);
+    });
+    Message.warn('warn', 0);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelectorAll(item).length).toBe(2);
+    });
+    Message.close();
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelectorAll(item).length).toBe(0);
+    });
+  });
+  test('should render when set duration', async () => {
+    const { container, rerender } = render(
+      <Button
+        onClick={() => {
+          Message.show('Test', 0);
+        }}
+      >
+        Test
+      </Button>,
+    );
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelector(wrapper)).toBeInTheDocument();
+    });
+    await waitFor(
+      async () => {
+        await delay(3200);
+        expect(document.querySelector(wrapper)).toBeInTheDocument();
+      },
+      { timeout: 10000 },
+    );
+    fireEvent.click(document.querySelector(closeClassName)!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelector(wrapper)).not.toBeInTheDocument();
+    });
+    rerender(
+      <Button
+        onClick={() => {
+          Message.show('Test', 5);
+        }}
+      >
+        Test
+      </Button>,
+    );
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelector(wrapper)).toBeInTheDocument();
+    });
+    await waitFor(
+      async () => {
+        await delay(5200);
+        expect(document.querySelector(wrapper)).not.toBeInTheDocument();
+      },
+      { timeout: 10000 },
+    );
+  });
+});
+describe('Message[Option]', () => {
+  test('should render when set className', async () => {
+    const { container } = render(
+      <Button
+        onClick={() => {
+          Message.show('Test', 5, {
+            className: 'demo',
+          });
+        }}
+      >
+        Test
+      </Button>,
+    );
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      classContentTest(document.querySelector(item)!, 'demo');
+    });
+    fireEvent.click(document.querySelector(closeClassName)!);
+    await waitFor(async () => {
+      await delay(200);
+      expect(document.querySelector(wrapper)).not.toBeInTheDocument();
+    });
+  });
+  test('should render when set container', async () => {
+    const { container } = render(
+      <div className='.demo'>
+        <Button
+          onClick={() => {
+            Message.show('Test', 5, {
+              container: document.querySelector('.demo') as HTMLElement,
+            });
+          }}
+        >
+          Test
+        </Button>
+      </div>,
+    );
+    fireEvent.click(container.querySelector('button')!);
+    await waitFor(async () => {
+      await delay(200);
+      screen.debug();
+    });
   });
 });
