131106fe1faaf6ee165115f1ce33b489289d9324
zyfwudi <leonardzhao@shein.com>
2023-11-19 16:27:33 +0800

    feat: add cypress test configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: false,
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:4000',
+    specPattern: `cypress/e2e/**/*.cy.{js,jsx,ts,tsx}`,
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
     },
   },
 });
