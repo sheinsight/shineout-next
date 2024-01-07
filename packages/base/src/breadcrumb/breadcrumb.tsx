6ab90166e8051b975876defa5bf24955c392f3e3
zyfwudi <leonardzhao@shein.com>
2024-01-07 15:25:39 +0800

    fix: simplify the logic

--- packages/base/src/breadcrumb/breadcrumb.tsx
+++ packages/base/src/breadcrumb/breadcrumb.tsx
@@ -64,17 +64,14 @@ const Breadcrumb = <Item = BreadcrumbDataType,>(props: BreadcrumbProps<Item>) =>
       {data &&
         getRenderData().map((d, index) => {
           const itemFirst = Array.isArray(d) ? d[0] : d;
-          const isLastItem = index === data.length - 1;
-          const shouldRenderSeparator = !props.max || (props.max && index !== props.max);
+          const isLastItem = index === (props.max !== undefined ? props.max : data.length - 1);
           return (
             <div
               className={breadcrumbClasses?.item}
               key={props.keygen ? getKey(props.keygen, itemFirst as Item, index) : index}
             >
               {Array.isArray(d) ? renderArray(d) : renderItem(d as Item)}
-              {!isLastItem && shouldRenderSeparator && (
-                <div className={breadcrumbClasses?.separator}>{separator}</div>
-              )}
+              {!isLastItem && <div className={breadcrumbClasses?.separator}>{separator}</div>}
             </div>
           );
         })}
