df2ad13fc5efca4614e56b753ab7d810e2814c67
YanYuHao <451122791@qq.com>
2024-01-10 20:37:34 +0800

    feat: add isBoolean util function

--- packages/hooks/src/utils/is.ts
+++ packages/hooks/src/utils/is.ts
@@ -17,6 +17,7 @@ export const isArray = Array.isArray;
 export const isObject = (val: unknown): val is ObjectType =>
   !!val && typeof val === 'object' && !isArray(val);
 export const isDate = (val: unknown): val is Date => val instanceof Date;
+export const isBoolean = (val: unknown): val is boolean => val instanceof Boolean;
 export const isError = (val: unknown): boolean => val instanceof Error;
 export const isNumber = (n: unknown): n is number => typeof n === 'number';
 export const isRegexp = (val: unknown): boolean => val instanceof RegExp;
