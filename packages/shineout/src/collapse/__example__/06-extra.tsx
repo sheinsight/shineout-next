0b3684eabfd6a33628bb46fc25395c609af073a8
zyfwudi <leonardzhao@shein.com>
2024-02-22 10:51:40 +0800

    fix: repair last value padding right

--- packages/shineout/src/collapse/__example__/06-extra.tsx
+++ packages/shineout/src/collapse/__example__/06-extra.tsx
@@ -5,7 +5,7 @@
  *    -- Customize the extra content of each panel
  */
 import React from 'react';
-import { Collapse, Checkbox } from 'shineout';
+import { Collapse, Checkbox, Button } from 'shineout';
 
 export default () => {
   return (
@@ -29,7 +29,7 @@ export default () => {
           just live out their joy and let it splash its sunlight and glow into other lives as
           naturally as bird sings.
         </Collapse.Item>
-        <Collapse.Item title='This is panel header 3' keygen='2'>
+        <Collapse.Item title='This is panel header 3' keygen='2' extra={<Button type='primary'>Button</Button>}>
           Joy in living comes from having fine emotions, trusting them, giving them the freedom of a
           bird in the open. Joy in living can never be assumed as a pose, or put on from the outside
           as a mask. People who have this joy do not need to talk about it; they radiate it. They
