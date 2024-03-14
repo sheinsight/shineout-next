de482e2519b0db98fc09010aa3ef9927f1d577d0
利浩 <chenlihao@shein.com>
2024-03-14 15:35:54 +0800

    change example

--- packages/shineout/src/form/__example__/011-fieldset-loop.tsx
+++ packages/shineout/src/form/__example__/011-fieldset-loop.tsx
@@ -36,16 +36,16 @@ const add = (
 );
 
 const cancel = (
-  <svg width='32' height='34' viewBox='0 0 32 34' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    <g clipPath='url(#clip0_2710_88188)'>
+  <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <g clipPath='url(#clip0_2710_88189)'>
       <path
-        d='M16.0001 9.66663C11.95 9.66663 8.66675 12.9499 8.66675 17C8.66675 21.05 11.95 24.3333 16.0001 24.3333C20.0502 24.3333 23.3334 21.05 23.3334 17C23.3334 12.9499 20.0502 9.66663 16.0001 9.66663ZM19.3334 17C19.3334 17.3681 19.0349 17.6666 18.6667 17.6666H13.3334C12.9652 17.6666 12.6667 17.3681 12.6667 17C12.6667 16.6318 12.9652 16.3333 13.3334 16.3333H18.6667C19.0349 16.3333 19.3334 16.6318 19.3334 17Z'
+        d='M8.00008 0.666626C3.94999 0.666626 0.666748 3.94987 0.666748 7.99996C0.666748 12.05 3.94999 15.3333 8.00008 15.3333C12.0502 15.3333 15.3334 12.05 15.3334 7.99996C15.3334 3.94987 12.0502 0.666626 8.00008 0.666626ZM11.3334 7.99996C11.3334 8.36815 11.0349 8.66663 10.6667 8.66663H5.33341C4.96522 8.66663 4.66675 8.36815 4.66675 7.99996C4.66675 7.63177 4.96522 7.33329 5.33341 7.33329H10.6667C11.0349 7.33329 11.3334 7.63177 11.3334 7.99996Z'
         fill='#EB4242'
       />
     </g>
     <defs>
-      <clipPath id='clip0_2710_88188'>
-        <rect width='16' height='16' fill='white' transform='translate(8 9)' />
+      <clipPath id='clip0_2710_88189'>
+        <rect width='16' height='16' fill='white' />
       </clipPath>
     </defs>
   </svg>
@@ -86,33 +86,35 @@ const App: React.FC = () => {
           defaultValue={[{ name: 'Hermione Granger', age: '16' }, {}]}
         >
           {({ onAppend, onRemove }) => (
-            <Form.Item style={{ display: 'flex', alignItems: 'center', marginBottom: 12 }}>
-              <Input
-                name='name'
-                placeholder='Name'
-                title='Friend name'
-                rules={[rules.required('Name is required')]}
-                style={{ width: 180, marginInlineEnd: 8 }}
-                clearable
-              />
-              <Input
-                name='age'
-                type='number'
-                placeholder='Age'
-                rules={[rules.required('Age is required')]}
-                title='Friend age'
-                style={{ width: 60 }}
-                clearable
-              />
-              <a
-                style={{ margin: '0 12px', lineHeight: 1, cursor: 'pointer' }}
-                onClick={() => onAppend({ name: '', age: '16' })}
-              >
-                {add}
-              </a>
-              <a style={{ lineHeight: 1, cursor: 'pointer' }} onClick={onRemove}>
-                {cancel}
-              </a>
+            <Form.Item style={{ marginBottom: 12 }}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Input
+                  name='name'
+                  placeholder='Name'
+                  title='Friend name'
+                  rules={[rules.required('Name is required')]}
+                  style={{ width: 180, marginInlineEnd: 8 }}
+                  clearable
+                />
+                <Input
+                  name='age'
+                  type='number'
+                  placeholder='Age'
+                  rules={[rules.required('Age is required')]}
+                  title='Friend age'
+                  style={{ width: 60 }}
+                  clearable
+                />
+                <a
+                  style={{ margin: '0 12px', lineHeight: 1, cursor: 'pointer' }}
+                  onClick={() => onAppend({ name: '', age: '16' })}
+                >
+                  {add}
+                </a>
+                <a style={{ lineHeight: 1, cursor: 'pointer' }} onClick={onRemove}>
+                  {cancel}
+                </a>
+              </div>
             </Form.Item>
           )}
         </Form.FieldSet>
