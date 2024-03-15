350d453266c63f8b9044e392eff3438b7f4607ce
利浩 <chenlihao@shein.com>
2024-03-15 18:01:45 +0800

    feat: 修复 fieldSet 中嵌套 form

--- packages/hooks/src/components/use-form/Provider.tsx
+++ packages/hooks/src/components/use-form/Provider.tsx
@@ -1,15 +1,19 @@
 import { FormBindContext } from './form-bind-context';
 import { FormFuncContext } from './form-func-context';
 import { FormConfigContext } from './form-config-context';
+import FieldSetContext from './use-form-fieldset/fieldset-context';
 import * as React from 'react';
 import { ProviderProps } from './use-form.type';
 
+const topPath = { path: '' };
 export const Provider = (props: ProviderProps) => {
   const { children, formConfig, formValue, formFunc } = props;
   return (
     <FormFuncContext.Provider value={formFunc}>
       <FormBindContext.Provider value={formValue}>
-        <FormConfigContext.Provider value={formConfig}>{children}</FormConfigContext.Provider>
+        <FieldSetContext.Provider value={topPath}>
+          <FormConfigContext.Provider value={formConfig}>{children}</FormConfigContext.Provider>
+        </FieldSetContext.Provider>
       </FormBindContext.Provider>
     </FormFuncContext.Provider>
   );
