be5a252089cb2fcff9a373347bdb2231a998b435
YanYuHao <451122791@qq.com>
2024-03-19 17:10:58 +0800

    feat: 调整 token 挂载方案

--- docs/theme/index.tsx
+++ docs/theme/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import Layout from './layout';
+import { setToken } from '@sheinx/theme';
 
 const App = () => {
+  useEffect(() => {
+    setToken({
+      selector: 'body',
+      tokenName: 'doc',
+    });
+  }, []);
   return <Layout></Layout>;
 };
 
