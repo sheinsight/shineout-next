1f9bedcab36c8f53e8a50eca28860026791474e9
利浩 <chenlihao@shein.com>
2024-03-26 10:16:51 +0800

    feat: 修改示例

--- packages/shineout/src/select/__example__/16-loading.tsx
+++ packages/shineout/src/select/__example__/16-loading.tsx
@@ -1,5 +1,5 @@
 /**
- * cn - 筛选限制字符长度
+ * cn - 加载中
  *    -- 设置`loading`为true时会展示一个默认的Spin组件，可以传入一个自定义的Spin代替
  * en - Loading
  *    -- Set `loading` to true will show a default Spin component, you can pass a custom Spin to replace it
