c1f3e44c3663629e3a0ba4ece0fa03bd9a5ec767
YanYuHao <451122791@qq.com>
2024-04-02 17:10:59 +0800

    feat: 更新 theme 包导出内容

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -3,136 +3,136 @@ import Token from './token/token';
 import setToken from './utils/token-setter';
 import { setConfig } from './config';
 
-import Alert from './alert';
+import Alert, { Alert as defaultAlert } from './alert';
 import { AlertTokens } from './alert/type';
 
-import Breadcrumb from './breadcrumb';
+import Breadcrumb, { Breadcrumb as defaultBreadcrumb } from './breadcrumb';
 import { BreadcrumbTokens } from './breadcrumb/type';
 
-import Button from './button';
+import Button, { Button as defaultButton } from './button';
 import { ButtonTokens } from './button/type';
 
-import Card from './card';
+import Card, { Card as defaultCard } from './card';
 import { CardTokens } from './card/type';
 
-import CardGroup from './card-group';
+import CardGroup, { CardGroup as defaultCardGroup } from './card-group';
 import { CardGroupTokens } from './card-group/type';
 
-import Carousel from './carousel';
+import Carousel, { Carousel as defaultCarousel } from './carousel';
 import { CarouselTokens } from './carousel/type';
 
-import Cascader from './cascader';
+import Cascader, { Cascader as defaultCascader } from './cascader';
 import { CascaderTokens } from './cascader/type';
 
-import Checkbox from './checkbox';
+import Checkbox, { Checkbox as defaultCheckbox } from './checkbox';
 import { CheckboxTokens } from './checkbox/type';
 
-import Collapse from './collapse';
+import Collapse, { Collapse as defaultCollapse } from './collapse';
 import { CollapseTokens } from './collapse/type';
 
-import Common from './common';
+import Common, { Common as defaultCommon } from './common';
 import { CommonTokens } from './common/type';
 
-import DatePicker from './date-picker';
+import DatePicker, { DatePicker as defaultDatePicker } from './date-picker';
 import { DatePickerTokens } from './date-picker/type';
 
-import Descriptions from './descriptions';
+import Descriptions, { Descriptions as defaultDescriptions } from './descriptions';
 import { DescriptionsTokens } from './descriptions/type';
 
-import Divider from './divider';
+import Divider, { Divider as defaultDivider } from './divider';
 import { DividerTokens } from './divider/type';
 
-import Dropdown from './dropdown';
+import Dropdown, { Dropdown as defaultDropdown } from './dropdown';
 import { DropdownTokens } from './dropdown/type';
 
-import EditableArea from './editable-area';
+import EditableArea, { EditableArea as defaultEditableArea } from './editable-area';
 import { EditableAreaTokens } from './editable-area/type';
 
-import Empty from './empty';
+import Empty, { Empty as defaultEmpty } from './empty';
 import { EmptyTokens } from './empty/type';
 
-import Form from './form';
+import Form, { Form as defaultForm } from './form';
 import { FormTokens } from './form/type';
 
-import Icon from './icon';
+import Icon, { Icon as defaultIcon } from './icon';
 import { IconTokens } from './icon/type';
 
-import Image from './image';
+import Image, { Image as defaultImage } from './image';
 import { ImageTokens } from './image/type';
 
-import Input from './input';
+import Input, { Input as defaultInput } from './input';
 import { InputTokens } from './input/type';
 
-import List from './list';
+import List, { List as defaultList } from './list';
 import { ListTokens } from './list/type';
 
-import Menu from './menu';
+import Menu, { Menu as defaultMenu } from './menu';
 import { MenuTokens } from './menu/type';
 
-import Message from './message';
+import Message, { Message as defaultMessage } from './message';
 import { MessageTokens } from './message/type';
 
-import Modal from './modal';
+import Modal, { Modal as defaultModal } from './modal';
 import { ModalTokens } from './modal/type';
 
-import Pagination from './pagination';
+import Pagination, { Pagination as defaultPagination } from './pagination';
 import { PaginationTokens } from './pagination/type';
 
-import Popover from './popover';
+import Popover, { Popover as defaultPopover } from './popover';
 import { PopoverTokens } from './popover/type';
 
-import Progress from './progress';
+import Progress, { Progress as defaultProgress } from './progress';
 import { ProgressTokens } from './progress/type';
 
-import Radio from './radio';
+import Radio, { Radio as defaultRadio } from './radio';
 import { RadioTokens } from './radio/type';
 
-import Rate from './rate';
+import Rate, { Rate as defaultRate } from './rate';
 import { RateTokens } from './rate/type';
 
-import Select from './select';
+import Select, { Select as defaultSelect } from './select';
 import { SelectTokens } from './select/type';
 
-import Slider from './slider';
+import Slider, { Slider as defaultSlider } from './slider';
 import { SliderTokens } from './slider/type';
 
-import Spin from './spin';
+import Spin, { Spin as defaultSpin } from './spin';
 import { SpinTokens } from './spin/type';
 
-import Steps from './steps';
+import Steps, { Steps as defaultSteps } from './steps';
 import { StepsTokens } from './steps/type';
 
-import Sticky from './sticky';
+import Sticky, { Sticky as defaultSticky } from './sticky';
 import { StickyTokens } from './sticky/type';
 
-import Switch from './switch';
+import Switch, { Switch as defaultSwitch } from './switch';
 import { SwitchTokens } from './switch/type';
 
-import Table from './table';
+import Table, { Table as defaultTable } from './table';
 import { TableTokens } from './table/type';
 
-import Tabs from './tabs';
+import Tabs, { Tabs as defaultTabs } from './tabs';
 import { TabsTokens } from './tabs/type';
 
-import Tag from './tag';
+import Tag, { Tag as defaultTag } from './tag';
 import { TagTokens } from './tag/type';
 
-import Textarea from './textarea';
+import Textarea, { Textarea as defaultTextarea } from './textarea';
 import { TextareaTokens } from './textarea/type';
 
-import Tooltip from './tooltip';
+import Tooltip, { Tooltip as defaultTooltip } from './tooltip';
 import { TooltipTokens } from './tooltip/type';
 
-import Transfer from './transfer';
+import Transfer, { Transfer as defaultTransfer } from './transfer';
 import { TransferTokens } from './transfer/type';
 
-import Tree from './tree';
+import Tree, { Tree as defaultTree } from './tree';
 import { TreeTokens } from './tree/type';
 
-import TreeSelect from './tree-select';
+import TreeSelect, { TreeSelect as defaultTreeSelect } from './tree-select';
 import { TreeSelectTokens } from './tree-select/type';
 
-import Upload from './upload';
+import Upload, { Upload as defaultUpload } from './upload';
 import { UploadTokens } from './upload/type';
 
 export type ThemeTokens = AlertTokens &
@@ -227,8 +227,55 @@ const Tokens: ThemeTokens = {
   ...Upload,
 };
 
+const defaultTokens: ThemeTokens = {
+  ...defaultAlert,
+  ...defaultBreadcrumb,
+  ...defaultButton,
+  ...defaultCard,
+  ...defaultCardGroup,
+  ...defaultCarousel,
+  ...defaultCascader,
+  ...defaultCheckbox,
+  ...defaultCollapse,
+  ...defaultCommon,
+  ...defaultDatePicker,
+  ...defaultDescriptions,
+  ...defaultDivider,
+  ...defaultDropdown,
+  ...defaultEditableArea,
+  ...defaultEmpty,
+  ...defaultForm,
+  ...defaultIcon,
+  ...defaultImage,
+  ...defaultInput,
+  ...defaultList,
+  ...defaultMenu,
+  ...defaultMessage,
+  ...defaultModal,
+  ...defaultPagination,
+  ...defaultPopover,
+  ...defaultProgress,
+  ...defaultRadio,
+  ...defaultRate,
+  ...defaultSelect,
+  ...defaultSlider,
+  ...defaultSpin,
+  ...defaultSteps,
+  ...defaultSticky,
+  ...defaultSwitch,
+  ...defaultTable,
+  ...defaultTabs,
+  ...defaultTag,
+  ...defaultTextarea,
+  ...defaultTooltip,
+  ...defaultTransfer,
+  ...defaultTree,
+  ...defaultTreeSelect,
+  ...defaultUpload,
+};
+
 export type { TokenType };
 export * from './hooks';
-export { Token, setToken, setConfig };
+export { Token, defaultTokens, setToken, setConfig };
 
 export default Tokens;
