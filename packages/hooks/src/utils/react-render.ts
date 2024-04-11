0dd4fc7dd1d394cb18ff0b475dc6528ff57a16e2
利浩 <chenlihao@shein.com>
2024-04-11 15:48:51 +0800

    fix: react render warning

--- packages/hooks/src/utils/react-render.ts
+++ packages/hooks/src/utils/react-render.ts
@@ -1,17 +1,16 @@
+//https://github.com/react-component/util/blob/master/src/React/render.ts
 import type * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import type { Root } from 'react-dom/client';
 
+// Let compiler not to search module usage
 const fullClone = {
   ...ReactDOM,
 } as typeof ReactDOM & {
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED?: {
+    usingClientEntryPoint?: boolean;
+  };
   createRoot: CreateRoot;
-};
-
-const MARK = '__sheinout_root__';
-
-type ContainerType = (Element | DocumentFragment) & {
-  [MARK]?: Root;
 };
 
 type CreateRoot = (container: ContainerType) => Root;
@@ -19,29 +18,55 @@ type CreateRoot = (container: ContainerType) => Root;
 const { version, render: reactRender, unmountComponentAtNode } = fullClone;
 
 let createRoot: CreateRoot;
-
 try {
   const mainVersion = Number((version || '').split('.')[0]);
   if (mainVersion >= 18) {
     ({ createRoot } = fullClone);
   }
 } catch (e) {
-  // Nothing;
+  // Do nothing;
 }
 
-const modernRender = (node: React.ReactElement, container: ContainerType) => {
+function toggleWarning(skip: boolean) {
+  const { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } = fullClone;
+
+  if (
+    __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED &&
+    typeof __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED === 'object'
+  ) {
+    __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.usingClientEntryPoint = skip;
+  }
+}
+
+const MARK = '__rc_react_root__';
+
+// ========================== Render ==========================
+type ContainerType = (Element | DocumentFragment) & {
+  [MARK]?: Root;
+};
+
+function modernRender(node: React.ReactElement, container: ContainerType) {
+  toggleWarning(true);
   const root = container[MARK] || createRoot(container);
+  toggleWarning(false);
 
   root.render(node);
 
   container[MARK] = root;
-};
+}
 
-const legacyRender = (node: React.ReactElement, container: ContainerType) => {
+function legacyRender(node: React.ReactElement, container: ContainerType) {
   reactRender(node, container);
-};
+}
 
-export const ReactRender = (node: React.ReactElement, container: ContainerType) => {
+/** @private Test usage. Not work in prod */
+function _r(node: React.ReactElement, container: ContainerType) {
+  if (process.env.NODE_ENV !== 'production') {
+    return legacyRender(node, container);
+  }
+}
+
+export function ReactRender(node: React.ReactElement, container: ContainerType) {
   // @ts-ignore
   if (createRoot) {
     modernRender(node, container);
@@ -49,24 +74,34 @@ export const ReactRender = (node: React.ReactElement, container: ContainerType) 
   }
 
   legacyRender(node, container);
-};
+}
 
-const modernUnmount = async (container: ContainerType) => {
+// ========================= Unmount ==========================
+async function modernUnmount(container: ContainerType) {
+  // Delay to unmount to avoid React 18 sync warning
   return Promise.resolve().then(() => {
     container[MARK]?.unmount();
 
     delete container[MARK];
   });
-};
+}
 
-const legacyUnmount = (container: ContainerType) => {
+function legacyUnmount(container: ContainerType) {
   unmountComponentAtNode(container);
-};
+}
 
-export const ReactUnmount = (container: ContainerType) => {
+/** @private Test usage. Not work in prod */
+function _u(container: ContainerType) {
+  if (process.env.NODE_ENV !== 'production') {
+    return legacyUnmount(container);
+  }
+}
+
+export async function ReactUnmount(container: ContainerType) {
   if (createRoot !== undefined) {
+    // Delay to unmount to avoid React 18 sync warning
     return modernUnmount(container);
   }
 
   legacyUnmount(container);
-};
+}
