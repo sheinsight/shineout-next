079fc26f60a41870d34908d1d8d1bc8f94d71f15
利浩 <chenlihao@shein.com>
2024-04-16 19:35:08 +0800

    feat: add hideArrow prop (#362)

--- packages/base/src/input/input-number.type.ts
+++ packages/base/src/input/input-number.type.ts
@@ -8,4 +8,10 @@ export interface InputNumberProps
   extends GetCommonProps<SimpleInputProps, NumValueType>,
     Omit<NumberProps, 'value' | 'onChange'> {
   jssStyle: InputStyle;
+  /**
+   * @en Whether to hide increase/decrease buttons
+   * @cn 是否隐藏增减按钮
+   * @default false
+   */
+  hideArrow?: boolean;
 }
