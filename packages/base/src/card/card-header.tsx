30b7e184802fd2903bd931b415944825373aa284
利浩 <chenlihao@shein.com>
2024-04-17 17:03:38 +0800

    feat: card support rtl

--- packages/base/src/card/card-header.tsx
+++ packages/base/src/card/card-header.tsx
@@ -2,19 +2,23 @@ import classNames from 'classnames';
 import { CardContext } from './card.context';
 import { useContext } from 'react';
 import Icons from '../icons';
+import { useConfig } from '../config';
 
 import type { CardHeaderProps } from './card-header.type';
 
 const CardHeader = (props: CardHeaderProps) => {
   const { align } = props;
   const cardClasses = props.jssStyle?.card?.();
+  const config = useConfig();
   const { collapsible, onCollapse, handleDragMouseDown } = useContext(CardContext);
 
   const renderIndicator = () => {
     if (!collapsible) return null;
     return (
       <div className={cardClasses?.indicator}>
-        <div className={cardClasses?.indicatorIcon}>{Icons.card.CollapseArrow}</div>
+        <div className={cardClasses?.indicatorIcon} dir={config.direction}>
+          {Icons.card.CollapseArrow}
+        </div>
       </div>
     );
   };
