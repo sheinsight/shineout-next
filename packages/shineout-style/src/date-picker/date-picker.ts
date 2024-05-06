431f7f8ac45beb5663f12782784ce36fa4a09762
利浩 <chenlihao@shein.com>
2024-05-06 16:56:28 +0800

    feat:  优化样式 datepicker 高度可以被撑开 (#410)
    * feat:  优化样式 datepicker 高度可以被撑开
    
    * feat: add changelog

--- packages/shineout-style/src/date-picker/date-picker.ts
+++ packages/shineout-style/src/date-picker/date-picker.ts
@@ -113,6 +113,7 @@ const datePickerStyle: JsStyles<DatePickerClassType> = {
   resultText: {
     lineHeight: token.lineHeightDynamic,
     position: 'relative',
+    zIndex: 0,
     flex: '1',
     minWidth: 0,
     '&::before': {
@@ -121,7 +122,25 @@ const datePickerStyle: JsStyles<DatePickerClassType> = {
     },
   },
   resultTextPadding: {
+    position: 'relative',
+    zIndex: 2,
+    '& > input': {
+      color: 'inherit',
+      padding: '0',
+      border: '0',
+      outline: '0',
+      fontSize: 'inherit',
+      lineHeight: token.lineHeightDynamic,
+      backgroundColor: 'transparent',
+      width: '100%',
+      '&::placeholder': {
+        color: token.datePickerPlaceholderColor,
+      },
+    },
+  },
+  resultTextBg: {
     position: 'absolute',
+    zIndex: 1,
     padding: `0 ${token.datePickerResultTextPaddingX}`,
     left: `calc(-1 * ${token.datePickerResultTextPaddingX})`,
     right: `calc(-1 * ${token.datePickerResultTextPaddingX})`,
@@ -143,7 +162,7 @@ const datePickerStyle: JsStyles<DatePickerClassType> = {
     },
   },
   resultTextActive: {
-    '& $resultTextPadding': {
+    '& $resultTextBg': {
       backgroundColor: token.datePickerResultTextActiveBackgroundColor,
     },
   },
