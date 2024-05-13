089e76508ab69bb11925a6b3a4d36ef1a5790cbf
利浩 <chenlihao@shein.com>
2024-05-13 19:42:28 +0800

    fix: 修复 datum.set 不支持传两个参数

--- packages/hooks/src/components/use-form/use-form-flow/use-form-datum.tsx
+++ packages/hooks/src/components/use-form/use-form-flow/use-form-datum.tsx
@@ -1,14 +1,24 @@
 import useLatestObj from '../../../common/use-latest-obj';
+import { usePersistFn } from '../../../common/use-persist-fn';
 import { useFormFunc } from '../form-func-context';
 
 export interface FormDatum {
-  set: (vals: { [name: string]: any }) => void;
+  set: (vals: { [name: string]: any } | string, value?: any) => void;
   get: (name?: string) => any;
 }
+
 const useFormDatum = () => {
   const formFunc = useFormFunc();
+
+  const set: FormDatum['set'] = usePersistFn((vals, value) => {
+    if (typeof vals === 'object') {
+      formFunc?.setValue(vals);
+    } else {
+      formFunc?.setValue({ [vals]: value });
+    }
+  });
   const func = useLatestObj({
-    set: formFunc?.setValue,
+    set: set,
     get: formFunc?.getValue,
   });
   const datum = formFunc ? func : undefined;
