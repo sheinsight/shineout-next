3323a9184ddd5676c27c6008bccd185baa036a9d
zyfwudi <leonardzhao@shein.com>
2024-05-13 20:47:00 +0800

    fix: leafClass is missing function type

--- packages/base/src/tree/tree-node.tsx
+++ packages/base/src/tree/tree-node.tsx
@@ -89,7 +89,7 @@ const Node = <DataItem, Value extends KeygenResult[]>(props: TreeNodeProps<DataI
 
   const rootClass = classNames(
     contentStyle.node,
-    isLeaf() && leafClass,
+    isLeaf() && (util.isString(leafClass) ? leafClass : util.isFunc(leafClass) && leafClass(data)),
     util.isString(nodeClass) && nodeClass,
     util.isFunc(nodeClass) && nodeClass(data),
     {
