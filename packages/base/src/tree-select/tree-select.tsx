e48641f9fdb20d76297c38e438c609b9a90641dd
YanYuHao <451122791@qq.com>
2024-05-14 10:58:49 +0800

    fix: 修复 TreeSelect 单选后节点没有高亮的问题

--- packages/base/src/tree-select/tree-select.tsx
+++ packages/base/src/tree-select/tree-select.tsx
@@ -147,6 +147,7 @@ const TreeSelect = <DataItem, Value extends TreeSelectValueType>(
     data,
     unmatch,
     disabled,
+    active: multiple ? undefined : value[0],
     childrenKey: childrenKey,
     keygen,
     onExpand,
@@ -599,7 +600,7 @@ const TreeSelect = <DataItem, Value extends TreeSelectValueType>(
     } else {
       treeProps.onClick = handleTreeClick;
       treeProps.renderItem = renderActive;
-      treeProps.active = value;
+      treeProps.active = value[0];
     }
     if ('expanded' in props) {
       treeProps.expanded = expanded;
@@ -618,6 +619,7 @@ const TreeSelect = <DataItem, Value extends TreeSelectValueType>(
           keygen={keygen}
           unmatch={unmatch}
           value={value}
+          highlight
           loader={loader}
           onExpand={onExpandTree}
           expanded={controlExpanded}
