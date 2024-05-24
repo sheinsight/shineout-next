a49ffcc769426be2055fa102f3a092a5b3337cce
利浩 <chenlihao@shein.com>
2024-05-24 14:04:32 +0800

    feat: Input.group 支持 status 设置错误状态

---
+++ packages/shineout/src/input/__example__/04-02-group-validate.tsx
@@ -0,0 +1,30 @@
+/**
+ * cn - Group 的校验
+ *    -- 配置 `status` 属性可以展示错误状态
+ * en - Front and rear tags
+ *    -- Use the built-in group component to add tags before and after
+ */
+
+import React from 'react';
+import { Input, Form, Rule } from 'shineout';
+
+const rules = Rule();
+
+const style: React.CSSProperties = { width: 300 };
+
+const App: React.FC = () => (
+  <Form>
+    <Form.Item label='URL' inline>
+      <Form.Field name='email' rules={[rules.required, rules.url('url is invalid')]}>
+        {({ value, error, onChange }) => (
+          <Input.Group style={style} status={error ? 'error' : undefined}>
+            <b>http://</b>
+            <Input value={value} onChange={onChange} placeholder='please enter url' />
+          </Input.Group>
+        )}
+      </Form.Field>
+    </Form.Item>
+  </Form>
+);
+
+export default App;
