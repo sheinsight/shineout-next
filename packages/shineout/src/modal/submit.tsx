3352264d5bfcfb4392ae7c783e6fcf2f3c2c34ec
利浩 <chenlihao@shein.com>
2024-05-27 10:16:21 +0800

    fix: 修复 Modal.Submit loading 样式缺失的问题

--- packages/shineout/src/modal/submit.tsx
+++ packages/shineout/src/modal/submit.tsx
@@ -1,9 +1,10 @@
 import { ModalSubmit } from '@sheinx/base';
-import { useButtonStyle } from '@sheinx/shineout-style';
+import { useButtonStyle, useSpinStyle } from '@sheinx/shineout-style';
 import { ButtonProps } from '../button/button.type';
 
 const jssStyle = {
   button: useButtonStyle,
+  spin: useSpinStyle,
 };
 
 const Submit = (props: ButtonProps) => {
