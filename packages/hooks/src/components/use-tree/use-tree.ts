8751ff3e5a22f92397bc7fda62cfe39534fdf19a
利浩 <chenlihao@shein.com>
2024-05-31 14:27:01 +0800

    fix: 修复 tree disabled 传入 bool 值变化后无效果 (#495)

--- packages/hooks/src/components/use-tree/use-tree.ts
+++ packages/hooks/src/components/use-tree/use-tree.ts
@@ -80,7 +80,6 @@ const useTree = <DataItem>(props: BaseTreeProps<DataItem>) => {
     valueMap: new Map<KeygenResult, CheckedStatusType>(),
     updateMap: new Map<KeygenResult, UpdateFunc>(),
     unmatchedValueMap: new Map<any, any>(),
-    disabled: false,
     value: undefined,
     data: [],
     cachedValue: [],
@@ -169,13 +168,13 @@ const useTree = <DataItem>(props: BaseTreeProps<DataItem>) => {
     return context.pathMap.get(id);
   };
 
-  const getDisabled = () => {
+  const getDisabled = usePersistFn(() => {
     if (isFunc(disabledProps)) {
       return disabledProps;
     }
 
-    return () => !!disabledProps;
-  };
+    return () => false;
+  });
 
   const getChecked = (id: KeygenResult) => {
     const value = get(id);
@@ -241,9 +240,8 @@ const useTree = <DataItem>(props: BaseTreeProps<DataItem>) => {
       context.dataMap.set(id, item);
 
       let isDisabled = !!disabled;
-
-      if (isDisabled === false && isFunc(context.disabled)) {
-        isDisabled = context.disabled(item);
+      if (isDisabled === false) {
+        isDisabled = getDisabled()(item);
       }
 
       const indexPath = [...index, i];
@@ -341,9 +339,12 @@ const useTree = <DataItem>(props: BaseTreeProps<DataItem>) => {
   };
 
   const isDisabled = (id: KeygenResult) => {
-    const node = context.pathMap.get(id);
-    if (node) return node.isDisabled;
-    return false;
+    if (isFunc(disabledProps)) {
+      const node = context.pathMap.get(id);
+      if (node) return node.isDisabled;
+      return false;
+    }
+    return !!disabledProps;
   };
 
   type CheckedStatus = 0 | 1 | 2;
@@ -365,7 +366,6 @@ const useTree = <DataItem>(props: BaseTreeProps<DataItem>) => {
     context.dataMap = new Map();
     context.valueMap = new Map();
     context.unmatchedValueMap = new Map();
-    context.disabled = getDisabled();
     context.data = toArray(data) as DataItem[];
     if (!data) return;
 
