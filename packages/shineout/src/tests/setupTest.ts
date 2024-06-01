b632e88bba080d36e2b6ee5fd972f7b60296996e
zyfwudi <leonardzhao@shein.com>
2024-06-01 13:36:53 +0800

    feat: className in test

--- packages/shineout/src/tests/setupTest.ts
+++ packages/shineout/src/tests/setupTest.ts
@@ -1,2 +1,17 @@
+import { setJssConfig } from '@sheinx/shineout-style';
+import { _convertCamelToDash } from './utils';
+
 import { setConfig, } from 'shineout';
 setConfig({ delay: 0 });
+
+setJssConfig({
+  generateId: (rule: any, sheet: any) => {
+    const options = sheet.options;
+    const ns = options.classNamePrefix;
+    if (!ns) {
+      console.warn('[sheinx/base]: styled should give namespace');
+    }
+    const prefix = 'soui-';
+    return `${prefix}${ns}${_convertCamelToDash(rule.key)}`;
+  },
+});
