bd38bb2d112a22895239e499b3a5f976fb4b3500
zyfwudi <leonardzhao@shein.com>
2024-07-03 17:22:33 +0800

    fix: style

---
+++ docs/theme/components/example/codesandbox/select-mock.ts
@@ -0,0 +1,26 @@
+const primitiveData = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet', 'pink'];
+
+const complexData = [
+  { id: 1, name: 'red' },
+  { id: 2, name: 'orange' },
+  { id: 3, name: 'yellow' },
+  { id: 4, name: 'green' },
+  { id: 5, name: 'cyan' },
+  { id: 6, name: 'blue' },
+  { id: 7, name: 'violet' },
+  { id: 8, name: 'pink' },
+  { id: 9, name: 'black' },
+  { id: 10, name: 'white' },
+  { id: 11, name: 'gray' },
+  { id: 12, name: 'brown' },
+  { id: 13, name: 'purple' },
+  { id: 14, name: 'indigo' },
+  { id: 15, name: 'magenta' },
+  { id: 16, name: 'beige' },
+  { id: 17, name: 'teal' },
+  { id: 18, name: 'salmon' },
+  { id: 19, name: 'olive' },
+  { id: 20, name: 'maroon' },
+];
+
+export { primitiveData, complexData };
