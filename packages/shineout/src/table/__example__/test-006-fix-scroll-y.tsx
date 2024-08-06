15e1dd7da89b77dc9bebe37214efa8133e0d6605
翟胜佳 <zhaishengjia@shein.com>
2024-08-06 16:35:55 +0800

    fix: 修复`Table`的虚拟列表场景下的合并行中内容闪烁的问题

--- packages/shineout/src/table/__example__/test-006-fix-scroll-y.tsx
+++ packages/shineout/src/table/__example__/test-006-fix-scroll-y.tsx
@@ -1,70 +1,98 @@
 /**
- * cn - fix-table-scroll
+ * cn - 合并行的虚拟滚动
  *    -- 修复表格滚动
  * en - fix-table-scroll
  *    -- fix table scroll
  */
 import React from 'react';
-import { Table, Tabs, TYPE } from 'shineout';
-import { user } from '@sheinx/mock';
+import { Table, TYPE } from 'shineout';
 
 interface TableRowData {
   id: number;
   time: string;
   start: string;
-  height: number;
-  salary: number;
-  office: string;
-  country: string;
-  office5: string;
-  position: string;
-  lastName: string;
-  firstName: string;
 }
+
 type TableColumnItem = TYPE.Table.ColumnItem<TableRowData>;
 
-const data: TableRowData[] = user.fetchSync(20);
-
-const columns: TableColumnItem[] = [
-  { title: 'id', render: 'id', width: 50 },
+const data: TableRowData[] = [
   {
-    title: 'First Name',
-    group: 'Name',
-    render: 'firstName',
-    width: 120,
+    id: 8850,
+    start: '2010-03-22',
+    time: '000',
   },
   {
-    title: 'Last Name',
-    fixed: 'left',
-    group: 'Name',
-    render: 'lastName',
-    width: 120,
+    id: 9656,
+    start: '2010-03-22',
+    time: '111',
   },
-  { title: 'Country', render: 'country' },
-  { title: 'Position', render: 'position' },
-  { title: 'Office', render: 'office' },
-  { title: 'Start Date', render: 'start' },
   {
-    title: 'Salary($)',
-    fixed: 'right',
-    align: 'right',
-    width: 100,
-    render: (d) => `${d.salary.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')}`,
+    id: 9652,
+    start: '2010-03-22',
+    time: '111',
+  },
+  {
+    id: 1263,
+    start: '2010-03-23',
+    time: '111',
+  },
+  {
+    id: 1487,
+    start: '2010-03-23',
+    time: '222',
+  },
+  {
+    id: 5844,
+    start: '2010-03-24',
+    time: '222',
+  },
+  {
+    id: 8620,
+    start: '2010-03-24',
+    time: '333',
+  },
+  {
+    id: 7323,
+    start: '2010-03-25',
+    time: '333',
+  },
+  {
+    id: 9831,
+    start: '2010-03-25',
+    time: '444',
+  },
+  {
+    id: 1230,
+    start: '2010-03-25',
+    time: '444',
+  },
+  {
+    id: 4014,
+    start: '2010-03-26',
+    time: '444',
   },
 ];
 
+const columns: TableColumnItem[] = [
+  {
+    title: 'id',
+    render: (row, index) => index,
+    width: 70,
+  },
+  {
+    title: 'Start Date',
+    width: 120,
+    render:(row, index) => <span style={{color: 'red'}}>闪烁的文本{index}</span>,
+    rowSpan: (a, b) => a.start === b.start,
+  },
+  { title: 'Time', render: 'time', rowSpan: (a, b) => a.time === b.time, },
+];
+
 const App: React.FC = () => {
-  const [activeTab, setActiveTab] = React.useState<string>('basic');
-  return <>
-  <Tabs active={activeTab} onChange={v => setActiveTab(v)}>
-    <Tabs.Panel id="basic" tab="基础">
-      <h1>基础数据</h1>
-    </Tabs.Panel>
-    <Tabs.Panel id="table" tab="表格">
-      <Table bordered keygen='id' width={1500} style={{ height: '88vh' }} columns={columns} data={data} onScroll={() => {}} />
-    </Tabs.Panel>
-  </Tabs>
-  </>
-}
+
+  return <div style={{height: 150}}>
+    <Table bordered height="100%" data={data} hover={false} keygen='id' columns={columns} rowsInView={7} virtual />
+  </div>
+};
 
 export default App;
