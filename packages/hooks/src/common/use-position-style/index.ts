2afaf35fd3305a69e0468dd7438961621cac1cb6
颜宇浩 <yanyuhao@shein.com>
2024-08-29 17:12:22 +0800

    fix: 修复 tooltip 默认值 auto 遮挡问题

--- packages/hooks/src/common/use-position-style/index.ts
+++ packages/hooks/src/common/use-position-style/index.ts
@@ -71,6 +71,7 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
   } = config || {};
   // 初次渲染无样式的时候， 隐藏展示
   const [style, setStyle] = useState<React.CSSProperties>(hideStyle);
+  const [arrayStyle, setArrayStyle] = useState<React.CSSProperties>({});
 
   const { current: context } = React.useRef({
     element: null as HTMLDivElement | null,
@@ -118,6 +119,7 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
         }
       }
     }
+
     return newPosition;
   };
 
@@ -144,6 +146,8 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
       position: 'absolute',
       zIndex,
     };
+    const arrayStyle: React.CSSProperties = {};
+
     if (fixedWidth) {
       const widthKey = fixedWidth === 'min' ? 'minWidth' : 'width';
       style[widthKey] = rect.width;
@@ -153,10 +157,12 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
     const containerRect = rootContainer.getBoundingClientRect();
     const bodyRect = (document.documentElement || document.body).getBoundingClientRect();
     const containerScrollBarWidth = rootContainer.offsetWidth - rootContainer.clientWidth;
+    const targetRect = popupElRef.current?.getBoundingClientRect();
     const containerScroll = {
       left: rootContainer.scrollLeft,
       top: rootContainer.scrollTop,
     } as DOMRect;
+
     context.containerRect = containerRect;
     context.containerScroll = containerScroll;
     if (verticalPosition.includes(targetPosition)) {
@@ -166,15 +172,27 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
       if (h === 'left') {
         style.left = rect.left - containerRect.left + containerScroll.left;
         style.transform = '';
+        arrayStyle.left = `8px`;
         if (adjust) {
           overRight = rect.left + context.popUpWidth - bodyRect.right + containerScrollBarWidth;
+          if (style.left < 0 && targetRect) {
+            style.left = 'auto';
+          }
         }
       } else if (h === 'right') {
         style.right =
           containerRect.right - rect.right + containerScrollBarWidth - containerScroll.left;
+
         style.left = 'auto';
         style.transform = '';
-        if (adjust) overLeft = bodyRect.left - (rect.right - context.popUpWidth);
+        arrayStyle.right = `8px`;
+        if (adjust) {
+          overLeft = bodyRect.left - (rect.right - context.popUpWidth);
+          if (style.right < 0 && targetRect) {
+            style.left = bodyRect.width - targetRect.width;
+            style.right = 'auto';
+          }
+        }
       } else {
         // 居中对齐
         style.left = rect.left + rect.width / 2 - containerRect.left + containerScroll.left;
@@ -188,6 +206,9 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
             containerScrollBarWidth;
           overLeft = bodyRect.left - (rect.left + rect.width / 2 - context.popUpWidth / 2);
         }
+        if (targetRect) {
+          arrayStyle.left = `${targetRect.width / 2 - 5.9}px`;
+        }
       }
       if (adjust) {
         // 调节左右溢出
@@ -196,45 +217,63 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
           (style.left as number) -= overRight;
           // 扣除触发器距离页面右侧的距离，以保证从右侧弹出的窗口最右边对齐触发器最右边
           (style.left as number) -= toRightDistance;
+          if (targetRect) {
+            arrayStyle.left = `${targetRect?.width - context.parentRect.width / 2 - 5.9}px`;
+          }
         }
         if (overLeft > 0) {
           (style.left as number) += overLeft;
+
+          arrayStyle.left = `${rect.width / 2 - 5.9}px`;
         }
       }
       if (v === 'bottom') {
         style.top = rect.bottom - containerRect.top + containerScroll.top + popupGap;
+        if (targetRect) {
+          arrayStyle.top = `${0}px`;
+        }
       } else {
+        
         style.top = rect.top - containerRect.top + containerScroll.top - popupGap;
         style.transform += 'translateY(-100%)';
+        if (targetRect) {
+          arrayStyle.top = `${targetRect.height - 5.9 - 4}px`;
+        }
       }
     } else if (horizontalPosition.includes(targetPosition)) {
       const [h, v] = targetPosition.split('-');
       if (v === 'top') {
         style.top = rect.top - containerRect.top + containerScroll.top;
         style.transform = '';
+        arrayStyle.top = `8px`;
       } else if (v === 'bottom') {
         style.top = rect.bottom - containerRect.top + containerScroll.top;
+        arrayStyle.bottom = `8px`;
         style.transform = 'translateY(-100%)';
       } else {
         // 居中对齐
         style.top = rect.top - containerRect.top + containerScroll.top + rect.height / 2;
+        if (targetRect) {
+          arrayStyle.top = `${targetRect.height / 2 - 5.9 / 2}px`;
+        }
         style.transform = 'translateY(-50%)';
       }
       if (h === 'right') {
         style.left = rect.right - containerRect.left + containerScroll.left + popupGap;
       } else {
         style.left = rect.left - containerRect.left + containerScroll.left - popupGap;
+        arrayStyle.right = `0px`;
         style.transform += ' translateX(-100%)';
       }
     } else if (position === 'cover') {
       style.top = rect.top - containerRect.top + containerScroll.top;
       style.left = rect.left - containerRect.left + containerScroll.left;
     }
-    return style;
+    return { style, arrayStyle };
   };
 
   const getAbsoluteStyle = (position: string) => {
-    if (!parentElRef.current) return hideStyle;
+    if (!parentElRef.current) return { style: hideStyle };
     const rect = context.parentRect;
     if (scrollElRef?.current) {
       const visibleRect = scrollElRef.current?.getBoundingClientRect() || {};
@@ -244,17 +283,18 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
         rect.right < visibleRect.left ||
         rect.left > visibleRect.right
       ) {
-        return hideStyle;
+        return { style: hideStyle };
       }
     }
-    const style = getAbsolutePositionStyle(rect, position);
-    return style;
+    const { style, arrayStyle } = getAbsolutePositionStyle(rect, position);
+    return { style, arrayStyle };
   };
 
   const getStyle = () => {
     let newStyle: React.CSSProperties = {};
+    let newArrayStyle: React.CSSProperties | undefined = {};
     const { position, absolute } = config || {};
-    if (!position || !show || !parentElRef.current) return style;
+    if (!position || !show || !parentElRef.current) return { newStyle: style };
     context.parentRect = parentElRef.current.getBoundingClientRect();
     if (adjust) {
       const popupInfo = getPopUpInfo(context.parentRect);
@@ -266,24 +306,27 @@ export const usePositionStyle = (config: PositionStyleConfig) => {
     if (!absolute) {
       newStyle = getPositionStyle(realPosition, { popupGap, zIndex, fixedWidth });
     } else {
-      newStyle = getAbsoluteStyle(realPosition)!;
+      const { style: nextStyle, arrayStyle: nextArrayStyle } = getAbsoluteStyle(realPosition)!;
+      newStyle = nextStyle;
+      newArrayStyle = nextArrayStyle;
     }
     // for animation
     if (realPosition.indexOf('top') === 0) {
       newStyle.transformOrigin = 'center bottom';
     }
-    return newStyle;
+    return { newStyle, newArrayStyle };
   };
 
   const updateStyle = usePersistFn(() => {
-    const newStyle = getStyle();
+    const { newStyle, newArrayStyle } = getStyle();
     if (newStyle && !shallowEqual(style, newStyle)) {
       setStyle(newStyle);
+      setArrayStyle(newArrayStyle || {});
     }
   });
 
   useEffect(updateStyle, [show, position, absolute, updateKey, fixedWidth]);
-  return style;
+  return { style, arrayStyle };
 };
 
 export default usePositionStyle;
