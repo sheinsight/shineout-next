34bc9dd00435cf0cb3ddb495b848b0cbb5e703c4
翟胜佳 <zhaishengjia@shein.com>
2024-09-09 20:33:38 +0800

    fix: 修复`Input.Number` 输入过程中的字符串格式(例如1.)触发onChange的问题

---
+++ packages/shineout/src/input/__example__/t-14-invalid-number.tsx
@@ -0,0 +1,36 @@
+/**
+ * cn - 非法number
+ *    -- 输入非法的数字时，Input.Number 不会触发 onChange 事件
+ * en - Invalid number
+ *    -- When an invalid number is entered, Input.Number will not trigger the onChange event
+ */
+import React from 'react';
+import { Button, Input } from 'shineout';
+
+export default () => {
+  const [value, setValue] = React.useState();
+
+  const onIncrease = () => {
+    setValue((v) => {
+      const next = Number(v || 0) + 1;
+      return next;
+    });
+  }
+  return (
+    <div style={{display: 'flex', gap: 12}}>
+      <Input.Number
+        width={300}
+        placeholder='input something'
+        value={value}
+        type="number"
+        onChange={v => {
+          console.log('======================')
+          console.log('outter onChange: >>', typeof v, v)
+          console.log('======================')
+          setValue(v);
+        }}
+      />
+      <Button onClick={onIncrease}>increase value</Button>
+    </div>
+  );
+};
