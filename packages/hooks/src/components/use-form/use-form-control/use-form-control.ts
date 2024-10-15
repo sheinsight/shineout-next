ca1fcf837f9a02a407f707535038a64186f7009a
颜宇浩 <yanyuhao@shein.com>
2024-10-15 19:36:01 +0800

    fix: 修复 `Form.Field` 在设置默认值情况下 value 改变后可能失效的问题

--- packages/hooks/src/components/use-form/use-form-control/use-form-control.ts
+++ packages/hooks/src/components/use-form/use-form-control/use-form-control.ts
@@ -85,7 +85,11 @@ export default function useFormControl<T>(props: BaseFormControlProps<T>) {
         setErrorState(error);
       }
       if (!shallowEqual(value, latestInfo.valueState)) {
-        setValueState(value);
+        if (value === undefined && defaultValue !== undefined) {
+          setValueState(defaultValue);
+        } else {
+          setValueState(value);
+        }
         latestInfo.valueState = value;
       }
     },
@@ -175,6 +179,7 @@ export default function useFormControl<T>(props: BaseFormControlProps<T>) {
           controlFunc.bind(n, dv[index], validateFiled, update);
         });
       } else {
+        console.log('bind', name, defaultValue);
         controlFunc.bind(name, defaultValue, validateFiled, update);
       }
     }
