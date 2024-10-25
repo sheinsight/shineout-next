234f27cff34d6daeb0bed70e847dd0219fba7bf1
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2024-10-25 11:36:33 +0800

    faet: `Cascader` 新增 `renderCompressed` 支持自定义渲染折叠内容 (#751)
    * feat: Cascader 支持虚拟列表
    
    * feat: 新增 virtual 属性
    
    * feat: Cascader 新增 renderCompressed 支持自定义折叠内容展示
    
    ---------
    
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- packages/shineout/src/index.ts
+++ packages/shineout/src/index.ts
@@ -61,4 +61,8 @@ export * from './deprecated';
 
 export * as TYPE from './type';
 
+<<<<<<< HEAD
 export default { version: '3.5.0-beta.5' };
+=======
+export default { version: '3.5.0-beta.4' };
+>>>>>>> 03905f65 (faet: `Cascader` 新增 `renderCompressed` 支持自定义渲染折叠内容 (#751))
