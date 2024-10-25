28d84f72c667342ddbb5899cbf59dce46b05e35f
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2024-10-25 11:36:33 +0800

    faet: `Cascader` 新增 `renderCompressed` 支持自定义渲染折叠内容 (#751)
    * feat: Cascader 支持虚拟列表
    
    * feat: 新增 virtual 属性
    
    * feat: Cascader 新增 renderCompressed 支持自定义折叠内容展示
    
    ---------
    
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- packages/base/src/select/result-more.type.ts
+++ packages/base/src/select/result-more.type.ts
@@ -1,3 +1,4 @@
+import { KeygenResult, UnMatchedData } from '@sheinx/hooks';
 import { SelectProps } from './select.type';
 
 export interface ResultMoreProps<DataItem, Value>
@@ -11,12 +12,15 @@ export interface ResultMoreProps<DataItem, Value>
     | 'value'
     | 'compressed'
     | 'compressedBound'
+    | 'renderCompressed'
     | 'compressedClassName'
     | 'disabled'
     | 'onFilter'
   > {
   data: React.ReactNode[];
+  datas?: (DataItem | UnMatchedData)[];
   more?: number;
   showNum?: number;
+  onRemove?: (item: DataItem | UnMatchedData, key?: KeygenResult, index?: number) => void;
   classes: { tag: string; moreWrapper: string };
 }
