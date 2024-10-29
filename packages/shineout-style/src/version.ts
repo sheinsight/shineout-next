4e7e5e17c521a56c29e74700e98b5a445e692cc2
颜宇浩 <yanyuhao@shein.com>
2024-10-29 10:22:23 +0800

    feat: 3.5.0-beta.5

--- packages/shineout-style/src/version.ts
+++ packages/shineout-style/src/version.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export default '3.5.0-beta.5';
-=======
-export default '3.5.0-beta.4';
->>>>>>> 03905f65 (faet: `Cascader` 新增 `renderCompressed` 支持自定义渲染折叠内容 (#751))
+export default '3.5.0-beta.6';
