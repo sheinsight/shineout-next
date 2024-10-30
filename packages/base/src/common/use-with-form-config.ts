98f6ca9a7cd889f934c6a07e4cd2ccc69a70ed03
zhaishengjia <zhaishengjia@shein.com>
2024-10-30 19:35:55 +0800

    feat: `Form` 新增 `reserveAble` 属性，用于控制表单项是否保留

--- packages/base/src/common/use-with-form-config.ts
+++ packages/base/src/common/use-with-form-config.ts
@@ -4,10 +4,12 @@ export const useWithFormConfig = <T>(props: T) => {
   const formConfig = useFormConfig();
   const size: BaseFormProps<any>['size'] = (props as any)?.size || formConfig.size;
   const disabled: boolean = formConfig.disabled || (props as any)?.disabled;
+  const reserveAble: boolean = (props as any)?.reserveAble ?? formConfig.reserveAble;
   return {
     ...props,
     size,
     disabled,
+    reserveAble,
   } as T;
 };
 
