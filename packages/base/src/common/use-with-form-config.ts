949dfa9cdbe5187a6df2832b32e32480d70a4100
saint3347 <zhaishengjia@shein.com>
2024-11-29 15:38:20 +0800

    fix: 修复Form的reserveAble遇到嵌套对象时,对深度字段的保持不生效的问题

--- packages/base/src/common/use-with-form-config.ts
+++ packages/base/src/common/use-with-form-config.ts
@@ -4,13 +4,11 @@ export const useWithFormConfig = <T>(props: T) => {
   const formConfig = useFormConfig();
   const size: BaseFormProps<any>['size'] = (props as BaseFormProps<any>)?.size || formConfig.size;
   const disabled: boolean | undefined = formConfig.disabled || (props as BaseFormProps<any>)?.disabled;
-  const reserveAble: boolean | undefined = (props as BaseFormProps<any>)?.reserveAble ?? formConfig.reserveAble;
 
   return {
     ...props,
     ...(size !== undefined && { size }),
     ...(disabled !== undefined && { disabled }),
-    ...(reserveAble !== undefined && { reserveAble }),
     ...(formConfig.formName !== undefined && { formName: formConfig.formName }),
   } as T
 };
