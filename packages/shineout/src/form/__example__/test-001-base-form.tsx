96da9a25517c4165274c745248373b682f6281be
颜宇浩 <yanyuhao@shein.com>
2024-12-02 10:22:49 +0800

    feat: 调整例子

--- packages/shineout/src/form/__example__/test-001-base-form.tsx
+++ packages/shineout/src/form/__example__/test-001-base-form.tsx
@@ -6,125 +6,6 @@
  */
 import { useEffect, useState } from 'react';
 import { Form, Input } from 'shineout';
-import { usePersistFn } from '@sheinx/hooks';
-
-const ComponentB1 = (props) => {
-  const { onAppend } = props;
-  useEffect(() => {
-    setTimeout(() => {
-      onAppend({
-        'b-1': 1,
-        'b-2': 2,
-      });
-    }, 10);
-
-    // onAppend({
-    //   'b-1': 1,
-    //   'b-2': 2,
-    // });
-
-    console.log('B1 mounted');
-  }, []);
-
-  return (
-    <Form.Item label='B1'>
-      <Form.FieldSet name='a.b1'>
-        {({}) => {
-          return (
-            <div>
-              <Input name='b-1'></Input>
-              <Input name='b-2'></Input>
-            </div>
-          );
-        }}
-      </Form.FieldSet>
-    </Form.Item>
-  );
-};
-
-const ComponentB2 = () => {
-  useEffect(() => {
-    console.log('B2 mounted');
-  }, []);
-
-  return (
-    <Form.Item label='B2'>
-      <Form.FieldSet name='a.spec_info_list' defaultValue={[{ 'b-1': 3, 'b-2': 4 }]}>
-        {({}) => {
-          return (
-            <div>
-              <Input name='b-1'></Input>
-              <Input name='b-2'></Input>
-            </div>
-          );
-        }}
-      </Form.FieldSet>
-    </Form.Item>
-  );
-};
-
-const ComponentB3 = () => {
-  useEffect(() => {
-    console.log('B3 mounted');
-  }, []);
-
-  return (
-    <Form.Item label='B3'>
-      <Form.FieldSet name='a.ccc' defaultValue={[{ 'b-1': 3, 'b-2': 4 }]}>
-        {({}) => {
-          return (
-            <div>
-              <Input name='b-1'></Input>
-              <Input name='b-2'></Input>
-            </div>
-          );
-        }}
-      </Form.FieldSet>
-    </Form.Item>
-  );
-};
-
-const ComponentB4 = () => {
-  useEffect(() => {
-    console.log('B4 mounted');
-  }, []);
-
-  return (
-    <Form.Item label='B4'>
-      <Form.FieldSet name='a.ddd' defaultValue={[{ 'b-1': 3, 'b-2': 4 }]}>
-        {({}) => {
-          return (
-            <div>
-              <Input name='b-1'></Input>
-              <Input name='b-2'></Input>
-            </div>
-          );
-        }}
-      </Form.FieldSet>
-    </Form.Item>
-  );
-};
-
-const ComponentB = (props) => {
-  return (
-    <div>
-      <ComponentB1 {...props}></ComponentB1>
-      <ComponentB2 {...props}></ComponentB2>
-      <ComponentB3 {...props}></ComponentB3>
-      <ComponentB4 {...props}></ComponentB4>
-    </div>
-  );
-};
-
-const ComponentA = (props) => {
-  return (
-    <Form.Item label='Component A'>
-      <Form.Field name='a'>
-        <ComponentB {...props}></ComponentB>
-      </Form.Field>
-    </Form.Item>
-  );
-};
 
 export default () => {
   const [value, setValue] = useState({
@@ -144,22 +25,9 @@ export default () => {
 
   const handleReset = () => {};
 
-  const handleAppend = usePersistFn((item: any) => {
-    const nextForm = {
-      a: {
-        ...value.a,
-        b1: [...value.a.b1],
-      },
-    };
-    nextForm.a.b1.push(item);
-    setValue(nextForm);
-  });
-
   return (
     <div>
       <Form value={value} onChange={handleChange} onReset={handleReset} onSubmit={handleSubmit}>
-        <ComponentA onAppend={handleAppend}></ComponentA>
-
         <Form.Item label='' style={{ marginTop: 32, marginBottom: 0 }}>
           <Form.Submit>Submit</Form.Submit>
           <Form.Reset>Reset</Form.Reset>
