2092cb400af0bda49bf4f47f0c099fc3c97189b0
颜宇浩 <yanyuhao@shein.com>
2024-12-18 14:56:18 +0800

    chore: 去除无效代码

--- docs/theme/index.tsx
+++ docs/theme/index.tsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
 import Layout from './layout';
-import { setToken } from 'shineout';
 
 const App = () => {
-  useEffect(() => {
-    setToken({
-      selector: '.custom-header',
-      token: {
-        'Brand-6': 'Success-5',
-      },
-    });
-  }, []);
-
   return <Layout></Layout>;
 };
 
