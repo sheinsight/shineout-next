0da81e8c7ce6bd5c5defecf2c60aa982e3727574
KMS-Bismarck <50952954+KMS-Bismarck@users.noreply.github.com>
2025-03-14 16:22:25 +0800

    fix: 修复 `Upload.Dragger` 子组件缺失的问题 (#996)
    * fix: 修复 `Upload.Dragger` 子组件缺失的问题
    
    ---------
    
    Co-authored-by: 颜宇浩 <yanyuhao@shein.com>

--- packages/shineout/src/upload/interface.ts
+++ packages/shineout/src/upload/interface.ts
@@ -1,4 +1,5 @@
 export type { UploadProps as Props, Validator } from './upload.type';
 export type { UploadImageProps as ImageProps } from './image.type';
+export type { UploadDraggerProps as DraggerProps } from './dragger.type';
 
 export type { UploadOptions as Options } from '@sheinx/hooks';
