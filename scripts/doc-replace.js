bd83ad6349a8c536ede052a02cb252762589078e
ityuany <519495771@qq.com>
2025-03-31 16:19:52 +0800

    优化 doc 的逻辑

--- scripts/doc-replace.js
+++ scripts/doc-replace.js
@@ -1,34 +1,26 @@
 const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
-const { requestToServer } = require('./doc-embedding');
+const { execSync } = require('child_process');
 
 function readFile(filePath) {
   return fs.readFileSync(filePath, 'utf8');
 }
 
+function createPnpmCommand(registry){
+  return function exec(command) {
+    if (registry) {
+      execSync(`${command} --registry=${registry}`, { stdio: 'inherit',encoding: 'utf-8' });
+    }else {
+      execSync(command, { stdio: 'inherit',encoding: 'utf-8' });
+    }
+  }
+}
+
+const execWithRegistry = createPnpmCommand(process.env.NPM_URL);
+
+const exec = createPnpmCommand();
+
 function writeFile(filePath, content) {
   fs.writeFileSync(filePath, content);
-}
-
-function execCommand(command) {
-  exec(command, (error, stdout) => {
-    if (error) {
-      console.error(error);
-      process.exit(1);
-      return;
-    }
-    console.log(stdout);
-  });
-}
-
-function updateNpmrc(npmrcPath, newLine) {
-  const npmrcFilePath = path.resolve(npmrcPath, '.npmrc');
-  const npmrcContent = readFile(npmrcFilePath);
-
-  const updatedNpmrcContent = npmrcContent + '\n' + newLine;
-
-  writeFile(npmrcFilePath, updatedNpmrcContent);
 }
 
 function updateDocsIndexFile(filePath, firstLine, lastLine) {
@@ -42,36 +34,32 @@ function updateDocsIndexFile(filePath, firstLine, lastLine) {
 function updateWebpackFile(filePath) {
   const fileContent = readFile(filePath);
 
-  const updatedFirstLine = "const AlitaWebpackPlugin = require('@alita/webpack-plugin').default\n" + fileContent;
+  const updatedFirstLine = `
+      const AlitaWebpackPlugin = require('@alita/webpack-plugin').default
+      ${fileContent}
+  `;
 
   const pluginToAdd = `new AlitaWebpackPlugin({
       name: 'shineout',
     }),`;
 
-  const updatedContent = updatedFirstLine.replace(/plugins: \[/, `plugins: [\n    ${pluginToAdd}`);
+  const updatedContent = updatedFirstLine.replace(/plugins: \[/, `plugins: [
+${pluginToAdd}`);
   
   writeFile(filePath, updatedContent);
 }
 
-const installDeps = async () => {
-  await updateNpmrc('./', `registry=${process.env.NPM_URL}`);
-  execCommand('pnpm install @alita/react@1.3.2 -w');
-  execCommand('pnpm install @alita/webpack-plugin@1.3.2 -D -w');
-}
 
-if (process.env.NODE_ENV === 'development') {
-  try {
-    updateDocsIndexFile('./docs/index.tsx', "import { createApp } from '@alita/react'", "export const { bootstrap, mount, unmount } = createApp(<App />)");
-  
-    updateWebpackFile('./webpack/config.doc.js')
-  
-    installDeps()
 
-    // if (process.env.EMBEDDING_URL) {
-    //   requestToServer();
-    // }
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+if (process.env.CI) {
+  updateDocsIndexFile('./docs/index.tsx', "import { createApp } from '@alita/react'", "export const { bootstrap, mount, unmount } = createApp(<App />)");
+
+  updateWebpackFile('./webpack/config.doc.js')
+
+  execWithRegistry('pnpm install @alita/react@1.3.2 -w');
+  execWithRegistry('pnpm install @alita/webpack-plugin@1.3.2 -D -w');
+  exec('pnpm run build:doc')
+}else {
+  console.log('skip doc build , only for ci')
 }
+ 
