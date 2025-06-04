34a686bf063170883ce60696719b4cbb4364042a
saint3347 <zhaishengjia@shein.com>
2025-06-04 13:47:43 +0800

    fix: 更新Grid组件以支持JSS样式，修复样式覆盖问题并提升兼容性

---
+++ packages/shineout-style/src/grid/index.ts
@@ -0,0 +1,7 @@
+import { styled } from '../jss-style';
+import gridStyle from './grid';
+
+const useGridStyle = styled(gridStyle, 'grid');
+
+export { gridStyle, useGridStyle };
+export default gridStyle;
