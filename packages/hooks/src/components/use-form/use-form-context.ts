09a6934ec5447b32980310c357b1a969b86ef2f8
saint3347 <zhaishengjia@shein.com>
2025-06-26 10:31:31 +0800

    fix: 优化 useFormRef 函数，使用 useState 替代 useRef 管理表单引用状态

--- packages/hooks/src/components/use-form/use-form-context.ts
+++ packages/hooks/src/components/use-form/use-form-context.ts
@@ -4,13 +4,8 @@ import { FormRef } from './use-form-context.type';
 export const FormContext = React.createContext<any>({});
 
 export function useFormRef<V>(): [FormRef<V>, ((formRef: FormRef<V>) => void)] {
-  const ref = React.useRef({});
-
-  const setFormRef = (formRef: FormRef<V>) => {
-    ref.current = formRef;
-  };
-
-  return [ref.current as FormRef<V>, setFormRef]
+  const [formRef, setFormRef] = React.useState<FormRef<V>>({} as FormRef<V>);
+  return [formRef, setFormRef]
 }
 
 export function useFormInstance<V>(): FormRef<V> {
