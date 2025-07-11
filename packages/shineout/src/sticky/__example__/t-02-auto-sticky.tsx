cd313f638dfc9cb4c22295a0def7f557b6611991
saint3347 <zhaishengjia@shein.com>
2025-07-11 16:11:59 +0800

    chore: 增加示例:触底后取消吸附，并且元素应用 justify-content: flex-end;

---
+++ packages/shineout/src/sticky/__example__/t-02-auto-sticky.tsx
@@ -0,0 +1,113 @@
+/**
+ * cn - 触底检测吸附
+ *    -- 触底后取消吸附，并且元素应用 justify-content: flex-end;
+ * en - Auto sticky to top after bottom
+ *    -- Auto sticky to top after bottom
+ */
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import { Sticky, Skeleton } from 'shineout';
+
+const App: React.FC = () => {
+  const targetRef = useRef<HTMLDivElement>(null);
+  const [isPlaceholderSticky, setIsPlaceholderSticky] = useState(false);
+  const [isTargetStikcy, setIsTargetStikcy] = useState(false);
+  const [placeholderTop, setPlaceholderTop] = useState(0);
+
+  useLayoutEffect(() => {
+    const scrollContainer = document.getElementById('document-body');
+    // 计算targetRef底部到scrollContainer element顶部的距离
+    if (targetRef.current && scrollContainer) {
+      setPlaceholderTop(targetRef.current.getBoundingClientRect().bottom - scrollContainer.getBoundingClientRect().top);
+    }
+  }, []);
+
+  const $image = <div style={{ height: 200, backgroundColor: '#ccc' }}>height: 200px</div>;
+
+  return (
+    <div
+      id='document-body'
+      style={{
+        height: 600,
+        overflow: 'auto',
+        backgroundColor: '#f4f5f8',
+        backgroundImage:
+          'linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff), ' +
+          'linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff)',
+        backgroundSize: '20px 20px',
+        backgroundPosition: '0 0, 10px 10px',
+        border: '1px solid #e0e0e0',
+      }}
+    >
+      <header
+        style={{
+          height: 100,
+          padding: 24,
+          margin: 0,
+          position: 'sticky',
+          top: 0,
+          backgroundColor: '#f7f8f9',
+          borderBottom: '1px solid #e0e0e0',
+        }}
+      >
+        height: 100px
+      </header>
+
+      <section
+        style={{
+          padding: 24,
+          display: 'flex',
+          backgroundColor: '#fff',
+        }}
+      >
+        <div style={{ display: 'flex', gap: 16, width: '100%' }}>
+          <div
+            style={{
+              width: '30%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Sticky top={125} scrollContainer='#document-body' onChange={setIsTargetStikcy}>
+              <div ref={targetRef} style={isPlaceholderSticky ? { opacity: 0, height: 0, pointerEvents: 'none' } : {}}>
+                {$image}
+              </div>
+            </Sticky>
+            {isTargetStikcy && (
+              <div style={isPlaceholderSticky ? {} : { opacity: 0, height: 0, pointerEvents: 'none' }}>
+                {$image}
+              </div>
+            )}
+          </div>
+          <div style={{ flex: 1, minWidth: 0 }}>
+            <Skeleton loading text={{ rows: 15, width: '100%' }} button={{ count: 2 }}>
+              <div>
+                I am the content after loading. I am the content after loading. I am the content
+                after loading.
+              </div>
+            </Skeleton>
+            <Sticky
+              scrollContainer='#document-body'
+              top={placeholderTop}
+              onChange={(isStikcy) => {
+                setIsPlaceholderSticky(isStikcy);
+              }}
+            >
+              <span />
+            </Sticky>
+          </div>
+        </div>
+      </section>
+
+      <main style={{ padding: 24, marginTop: 24, marginBottom: 24, background: '#fff' }}>
+        {
+          Array.from({ length: 100 }).map((_, index) => (
+            <p key={index}>我是正文描述信息……</p>
+          ))
+        }
+      </main>
+    </div>
+  );
+};
+
+export default App;
