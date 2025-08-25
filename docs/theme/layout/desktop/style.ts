e2bb995ee559ffa74c72b8a0df693fb5daf24b25
Tom Zhai <zhaishengjia@shein.com>
2025-08-25 15:09:27 +0800

    docs: remove unused style

--- docs/theme/layout/desktop/style.ts
+++ docs/theme/layout/desktop/style.ts
@@ -37,57 +37,6 @@ export default createUseStyles(
       },
     },
 
-    nav: {
-      position: 'sticky',
-      top: 0,
-      height: navHeight,
-      width: '100%',
-      display: 'flex',
-      backgroundColor: 'var(--soui-neutral-fill-1)',
-      borderBottom: '1px solid var(--soui-neutral-border-1)',
-      zIndex: 2,
-      '& .left-nav': {
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 24px',
-        position: 'relative',
-        '&::after': {
-          content: '" "',
-          position: 'absolute',
-          right: 0,
-          width: 1,
-          height: 24,
-          top: 18,
-          backgroundColor: 'var(--soui-neutral-border-1)',
-        },
-      },
-      '& .logo': {
-        display: 'flex',
-        alignItems: 'center',
-        marginLeft: 24,
-        cursor: 'pointer',
-      },
-      '& .entry': {
-        // gap: 12,
-        width: '100%',
-        fontWeight: '700',
-        display: 'flex',
-        listStyle: 'none',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 16px',
-        '& li': {
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          '&:hover': {
-            color: '#999999',
-          },
-        },
-      },
-    },
-
     main: {
       width: '100%',
       height: '100%',
