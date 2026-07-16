8dff7bcf60020c25b6a4aa808d0eb796830c8446
Tom Zhai <zhaishengjia@shein.com>
2026-07-16 16:56:34 +0800

    refactor: remove unused SemanticKey/ClassNamesInfo exports from interface.ts

--- packages/shineout/src/popover/interface.ts
+++ packages/shineout/src/popover/interface.ts
@@ -1,7 +1,5 @@
 export type {
   PopoverProps as Props,
   PopoverPosition as Position,
-  PopoverSemanticKey as SemanticKey,
-  PopoverClassNamesInfo as ClassNamesInfo,
 } from './popover.type';
 export type { PopoverConfirmProps as ConfirmProps } from './confirm.type';
