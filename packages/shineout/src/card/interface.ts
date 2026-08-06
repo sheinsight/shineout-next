b02b59d1784abf4f26f6e51d9489f7f8aa215699
Tom Zhai <zhaishengjia@shein.com>
2026-08-06 20:39:29 +0800

    chore: export SemanticKey types from shineout interface files

--- packages/shineout/src/card/interface.ts
+++ packages/shineout/src/card/interface.ts
@@ -2,3 +2,4 @@ export type { CardProps as Props } from './card.type';
 export type { CardHeaderProps as HeaderProps } from './header.type';
 export type { CardBodyProps as BodyProps } from './body.type';
 export type { CardFooterProps as FooterProps } from './footer.type';
+export type { CardSemanticKey } from '@sheinx/base';
--- packages/shineout/src/pagination/interface.ts
+++ packages/shineout/src/pagination/interface.ts
@@ -1,1 +1,2 @@
 export type { PaginationProps as Props, TextParams } from './pagination.type';
+export type { PaginationSemanticKey } from '@sheinx/base';
