1fa6c38e377e963a10fdcddcb47d3d55b14cbfce
Tom Zhai <zhaishengjia@shein.com>
2026-08-06 19:34:15 +0800

    feat(collapse): support Semantic DOM API (classNames / styles / setConfig fallback)

--- packages/base/src/collapse/index.ts
+++ packages/base/src/collapse/index.ts
@@ -1,5 +1,5 @@
 export { default, default as Collapse } from './collapse';
-export type { CollapseProps, CollapseClasses } from './collapse.type';
+export type { CollapseProps, CollapseClasses, CollapseSemanticKey } from './collapse.type';
 
 export { default as CollapseItem } from './collapse-item';
 export type { CollapseItemProps, CollapseItemClasses } from './collapse-item.type';
